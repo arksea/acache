8458b19f183e528cb36513db1de5e218188864ee
xiaohaixing <xiaohaixing@felink>
2017-03-26 23:19:21 +0800

    默认配置移到接口默认方法

--- acache/src/main/java/net/arksea/acache/ICacheConfig.java
+++ acache/src/main/java/net/arksea/acache/ICacheConfig.java
@@ -4,17 +4,21 @@ package net.arksea.acache;
  *
  * Created by arksea on 2016/11/17.
  */
-public interface ICacheConfig {
+public interface ICacheConfig<TKey> {
     /**
      * 缓存闲置时间，单位秒，超过此时间没有访问将被从内存里清除
      * @return
      */
-    long getIdleTimeout();
+    default long getIdleTimeout() {
+        return 0;
+    };
     /**
      * 缓存激活时间，单位秒，超过此时间CacheActor将调用IDataSource接口取新数据
      * @return
      */
-    long getLiveTimeout();
+    default long getLiveTimeout(TKey key) {
+        return 300000; //默认缓存过期时间5分钟
+    }
 
 
     /**
@@ -24,18 +28,24 @@ public interface ICacheConfig {
      * request调用返回实际的结果后将重置退避时间
      * @return
      */
-    long getMaxBackoff();
+    default long getMaxBackoff() {
+        return 300000; //默认最大退避时间5分钟
+    }
 
     /**
      * 集群统一通过Master节点调用IDataSource.request()更新缓存
      * @return
      */
-    boolean updateByMaster();
+    default boolean updateByMaster() {
+        return false;
+    }
 
     /**
      * 更新缓存时是否自动同步到集群其他节点
      * @return
      */
-    boolean autoSync();
+    default boolean autoSync() {
+        return false;
+    }
 }
 
