dc92603a9e09137ae97fd6c9c27772e4403d5ad0
xiaohaixing <xiaohaixing@felink>
2017-06-16 15:58:44 +0800

    bug： 汗，居然没有定义成线程安全Map，会有内存可见性问题的

--- acache/src/main/java/net/arksea/acache/CacheActorState.java
+++ acache/src/main/java/net/arksea/acache/CacheActorState.java
@@ -2,13 +2,14 @@ package net.arksea.acache;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 保存CacheActor的配置、状态与缓存的数据，可以在CacheActor异常重启后继承
  * Created by arksea on 2016/11/17.
  */
 public class CacheActorState<TKey, TData> {
-    public final Map<TKey, CachedItem<TKey,TData>> cacheMap = new HashMap<>();
+    public final Map<TKey, CachedItem<TKey,TData>> cacheMap = new ConcurrentHashMap<>();
     public final ICacheConfig config;
     public final IDataSource<TKey,TData> dataSource;
     public CacheActorState(final ICacheConfig config, IDataSource<TKey,TData> dataSource) {
