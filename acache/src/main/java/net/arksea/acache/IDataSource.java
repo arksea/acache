a772ab3405f605f55912dedb715be23f154ff189
xiaohaixing <arksea@gmail.com>
2017-08-07 16:01:22 +0800

    bug：少了括号

--- acache/src/main/java/net/arksea/acache/IDataSource.java
+++ acache/src/main/java/net/arksea/acache/IDataSource.java
@@ -14,6 +14,7 @@ public interface IDataSource<TKey, TData> {
     Future<TimedData<TData>> request(TKey key);
     default void preStart(ActorRef cacheActor,String cacheName) {
         //default donothing
+    }
     /**
      * 此处定义的是阻塞式的同步初始化，需要异步初始化可以不实现此接口，在外部通过调用GetData请求进行初始化
      * @param keys
