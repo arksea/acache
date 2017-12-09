1555557bf231b8a1d7864ada9c0c843413b11ea8
xiaohaixing <arksea@gmail.com>
2017-12-09 21:48:40 +0800

    增加对无数据key处理逻辑的注释

--- acache/src/main/java/net/arksea/acache/IDataSource.java
+++ acache/src/main/java/net/arksea/acache/IDataSource.java
@@ -10,6 +10,11 @@ import java.util.Map;
  * Created by arksea on 2016/11/17.
  */
 public interface IDataSource<TKey, TData> extends ICacheConfig<TKey> {
+    /**
+     *
+     * @param key
+     * @return 返回null将被认为所请求的key没有对应的数据，会直接返回错误给请求方，不会作为错误记录日志
+     */
     Future<TimedData<TData>> request(TKey key);
     /**
      * 此处定义的是阻塞式的同步初始化，需要异步初始化可以通过GetData请求进行初始化
