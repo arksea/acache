4f0f498e3866d9922c4a2afa1c4fad9dd8f29f47
xiaohaixing <arksea@gmail.com>
2018-01-09 16:28:59 +0800

    bug: 创建pool时会使用相同的state

--- acache/src/main/java/net/arksea/acache/CacheActor.java
+++ acache/src/main/java/net/arksea/acache/CacheActor.java
@@ -18,9 +18,10 @@ public class CacheActor<TKey, TData> extends AbstractCacheActor {
     //Actor重启时继承原Actor状态与缓存
     public static <TKey,TData> Props props(final ICacheConfig config, final IDataSource<TKey,TData> dataRequest) {
         return Props.create(CacheActor.class, new Creator<CacheActor>() {
-            CacheActorState<TKey,TData> state = new CacheActorState<>(config,dataRequest);
             @Override
             public CacheActor<TKey, TData> create() throws Exception {
+                //此state必须放在每次创建实例时，否则会引起建立pooled cache时使用同一个state
+                CacheActorState<TKey,TData> state = new CacheActorState<>(config,dataRequest);
                 return new CacheActor<>( state);
             }
         });
